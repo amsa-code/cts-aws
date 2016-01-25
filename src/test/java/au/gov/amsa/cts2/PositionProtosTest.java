89a3871eb2d7542719b6fd5e4921b3d22c49021b
Dave Moten <davidmoten@gmail.com>
2016-01-25 12:54:19 +1100

    dev

---
+++ src/test/java/au/gov/amsa/cts2/PositionProtosTest.java
@@ -0,0 +1,17 @@
+package au.gov.amsa.cts2;
+
+import org.junit.Test;
+
+import au.gov.amsa.cts2.proto.PositionProtos.Position;
+
+public class PositionProtosTest {
+
+    @Test
+    public void test() {
+        byte[] bytes = Position.newBuilder().setIdentifierType(1).setValueInteger(123456789L).setLatitude(-35f)
+                .setLongitude(142f).setTimeEpochMs(System.currentTimeMillis()).setHeadingDegrees(27)
+                .setCourseDegrees(25).setSpeedKnots(12.0f).build().toByteArray();
+        System.out.println(bytes.length);
+    }
+
+}
