1f16afbef6fcfd4763b504fd07c2ad4b68c0cfd4
Dave Moten <davidmoten@gmail.com>
2016-01-25 13:47:10 +1100

    dev

--- src/test/java/au/gov/amsa/cts2/PositionProtosTest.java
+++ src/test/java/au/gov/amsa/cts2/PositionProtosTest.java
@@ -8,10 +8,18 @@ public class PositionProtosTest {
 
     @Test
     public void test() {
-        byte[] bytes = Position.newBuilder().setIdentifierType(1).setValueInteger(123456789L).setLatitude(-35f)
-                .setLongitude(142f).setTimeEpochMs(System.currentTimeMillis()).setHeadingDegrees(27)
-                .setCourseDegrees(25).setSpeedKnots(12.0f).build().toByteArray();
-        System.out.println(bytes.length);
+        {
+            Position p = Position.newBuilder().setIdentifierType(1).setValueInteger(123456789L)
+                    .setLatitude(-35f).setLongitude(142f).setTimeEpochMs(System.currentTimeMillis())
+                    .setHeadingDegrees(27).setCourseDegrees(25).setSpeedKnots(12.0f).build();
+            System.out.println(p.toByteArray().length);
+        }
+        {
+            Position p = Position.newBuilder().setIdentifierType(1).setValueInteger(123456789L)
+                    .setLatitude(-35f).setLongitude(142f).setTimeEpochMs(System.currentTimeMillis())
+                    .build();
+            System.out.println(p.toByteArray().length);
+        }
     }
 
 }
